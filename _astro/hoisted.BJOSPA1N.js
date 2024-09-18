6052643b169ffb1adcab30f1bf3febc6ee0a6258
I.Zhuravlev <freeeg@gmail.com>
2024-09-19 07:05:12 +0800

    mfti preview

--- _astro/hoisted.BJOSPA1N.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.Bawn_8nk.js";import"./hoisted.B1YAa4eq.js";import"./hoisted.DsZmShrG.js";
--- _astro/hoisted.DsZmShrG.js
+++
@@ -1,1 +0,0 @@
-const e=document.getElementById("hamburger");e?.addEventListener("click",()=>{e.dataset.active==="true"?delete e.dataset.active:e.dataset.active="true"});
